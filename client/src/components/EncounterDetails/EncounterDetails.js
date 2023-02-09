f0e4fb3e61676faeeb701f92bf9fe509f58df252
canyapalak <canyapalak@gmail.com>
2023-02-09 13:05:15 +0100

    🎨 improving the details page

--- client/src/components/EncounterDetails/EncounterDetails.js
+++ client/src/components/EncounterDetails/EncounterDetails.js
@@ -1,6 +1,10 @@
 import "./EncounterDetails.css";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import PlaceholderAvatar from "../assets/avatar-placeholder.png";
+import MapIcon from "../../components/assets/map-icon.png";
 
 function EncounterDetails() {
   const [oneEncounter, setOneEncounter] = useState("");
@@ -22,7 +26,7 @@ function EncounterDetails() {
         const urlFetchEncounterById = `http://localhost:5000/api/encounters/${_id}`;
         const response = await fetch(urlFetchEncounterById);
         const results = await response.json();
-        console.log("results :>> ", results);
+        // console.log("results :>> ", results);
 
         setOneEncounter(results.requestedId[0]);
       } catch (error) {
@@ -31,14 +35,56 @@ function EncounterDetails() {
       }
     };
 
-    console.log("oneEncounter :>> ", oneEncounter);
-
     fetchEncounterById();
   }, []);
+  console.log("oneEncounter :>> ", oneEncounter);
   return (
-    <div>
-      <p>{oneEncounter.username}</p>
-      <p>{oneEncounter.experience}</p>
+    <div className="details-container">
+      <Card className="details-card">
+        <span className="card-img">
+          <img src={oneEncounter.image} alt="Encounter Image" />
+        </span>
+        <div className="avatar-username-and-post-time">
+          <div className="avatar-and-username">
+            <span className="avatar">
+              <img src={PlaceholderAvatar} alt="Avatar" />
+            </span>
+            <span className="details-username">
+              <p>{oneEncounter.username}</p>
+            </span>
+          </div>
+          <span className="details-post-time">
+            <p>{postTime(oneEncounter.posttime)}</p>
+          </span>
+        </div>
+        <hr />
+        <div className="info-and-map">
+          <div className="species-location-coordination">
+            <span className="species">
+              <p className="small-title">species: &nbsp;</p>
+              <p>{oneEncounter.species}</p>
+            </span>
+            <span className="location">
+              <p className="small-title">location: &nbsp;</p>
+              <p>
+                {oneEncounter && oneEncounter.location
+                  ? `${oneEncounter.location.area}, ${oneEncounter.location.province}, ${oneEncounter.location.country}`
+                  : "-"}
+              </p>
+            </span>
+            <span className="coordinates">
+              <p className="small-title">coordinates: &nbsp;</p>
+              <p>
+                {oneEncounter && oneEncounter.coordinates
+                  ? `${oneEncounter.coordinates.latitude}, ${oneEncounter.coordinates.longitude}`
+                  : "-"}{" "}
+              </p>
+            </span>
+          </div>
+          <img src={MapIcon} alt="Map" />
+        </div>
+        <hr />
+      </Card>
     </div>
   );
 }
