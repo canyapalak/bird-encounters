da6f479100fe434853584bacaae8ee61c685ec27
canyapalak <canyapalak@gmail.com>
2023-03-10 12:36:16 +0100

    vercel

--- client/src/utils/serverURL.js
+++ client/src/utils/serverURL.js
@@ -1,5 +1,5 @@
 const serverURL =
-  process.env.NODE.ENV === "production"
+  process.env.NODE_ENV === "production"
     ? "https://bird-encounters-backend.vercel.app/"
     : "http://localhost:5000/";
 
